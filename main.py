8bd15bb2a17977162a96194753d9beba79b1e6a3
Mitchell Meier <mdmfvz@mst.edu>
2022-04-13 21:12:41 -0500

    bug fixes

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 from pathlib import Path
+from nltk.stem.wordnet import WordNetLemmatizer
 import discord
 import json
 import nltk
+
 import random
 
 API_KEYS = Path(__file__).parent / "keys.json"
-SUS = discord.File("sussy.gif")
+SUSSY = Path(__file__).parent / "sussy.gif"
 
 with open(API_KEYS, "r") as f:
   keys = json.load(f)
-
 
 bot = discord.Client()
 
 @bot.event
 async def on_message(message):
-  tokens = nltk.word_tokenize(message.content)
+  temp = nltk.word_tokenize(message.content)
+  tokens = nltk.pos_tag(temp)
   for index in range(len(tokens)-1,-1,-1):
     if "VB" in tokens[index][1]:
-      if random.randint(0,4) == 0:
-        await message.channel.send(tokens[index][0].upper() + " THIS", file=SUS)
+      if random.randint(0,9) == 0:
+        with open(SUSSY, "rb") as f:
+          base_word = WordNetLemmatizer().lemmatize(tokens[index][0],'v')
+          await message.channel.send(base_word.upper() + " THIS", file=discord.File(f))
       return
 
 bot.run(keys["BOT"])
